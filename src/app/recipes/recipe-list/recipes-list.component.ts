e30db0cd3f4405208f50090703b277150c79b7c7
Eduard Toma <eduard.daniel.toma@gmail.com>
2020-04-05 18:17:43 +0300

    added structure of recipe-component(list, item), shopping-list(edit,list) + models(recipe, igredient)

--- src/app/recipes/recipe-list/recipes-list.component.ts
+++ src/app/recipes/recipe-list/recipes-list.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2017/03/23/19/57/asparagus-2169305_1280.jpg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/04/21/22/50/pizza-1344720_1280.jpg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/11/29/13/02/cheese-1869708_1280.jpg'),
+  ];
 
   constructor() { }
 
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Igredient } from '../shared/igredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+  igredients: Igredient[] = [
+    new Igredient('Apples', 5),
+    new Igredient('Tomatoes', 10),
+  ];
 
   constructor() { }
 
