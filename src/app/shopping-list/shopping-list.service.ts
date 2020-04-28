a3740cb117d39678b0cbc1145785f9fc765861fc
Eduard Toma <eduard.daniel.toma@gmail.com>
2020-04-28 16:26:12 +0300

    externalize router configuration

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -5,8 +5,6 @@ export class ShoppingListService {
 
     ingredientsChanged = new EventEmitter<Ingredient[]>();
 
-    // @Input() recipe: Recipe;
-
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
         new Ingredient('Tomatoes', 10),
